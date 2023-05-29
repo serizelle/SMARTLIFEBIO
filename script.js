48f7fcda9da4ac9ae20b03516f5f855161d544ed
serizelle <sailorelena@gmail.com>
2023-05-29 14:15:01 +0300

    mobile done

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const menuEl = document.querySelector('.menu__burger');
 burgerEl.addEventListener('click', function (e) {
     menuEl.classList.toggle('menu_show');
 });
+
+const toggles = document.querySelectorAll('.toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
